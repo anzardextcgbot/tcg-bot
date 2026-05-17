8fa46a6891fd5416fd1c09bc360eac367607ac21
Anzar <anzargaming@outlook.de>
2026-05-17 14:56:27 +0200

    auto select single result

--- bot.py
+++ bot.py
@@ -227,7 +227,34 @@ async def preis(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 )
             ]
         )
+if len(cards) == 1:
+    card = cards[0]
 
+    image = card.get("images", {}).get("large")
+
+    name = card.get("name")
+    set_name = card.get("set", {}).get("name")
+    number = card.get("number")
+
+    price = (
+        card.get("cardmarket", {})
+        .get("prices", {})
+        .get("trendPrice", "?")
+    )
+
+    text = (
+        f"🃏 {name}\n"
+        f"📦 Set: {set_name}\n"
+        f"#️⃣ Nummer: {number}\n"
+        f"💰 Preis: {price} €"
+    )
+
+    await update.message.reply_photo(
+        photo=image,
+        caption=text
+    )
+
+    return
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     await update.message.reply_text(
